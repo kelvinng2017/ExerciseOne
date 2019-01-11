312d11adbe47bed5492b20f20fe9180323b31e6c
kelvinng <kelvinngshuhui@gmail.com>
2019-01-11 15:45:01 +0800

    完成登錄功能

--- app/src/main/java/com/example/kelvinng/exerciseone/Email_Login.java
+++ app/src/main/java/com/example/kelvinng/exerciseone/Email_Login.java
@@ -6,11 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Email_Login extends AppCompatActivity {
 
     Toolbar activity_email_login_toolbar;
     ActionBar activity_email_login_actionbar;
+    Button confirm_button_of_activity_email_register;
+    EditText email_login_edit_text,password_login_edit_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +27,20 @@ public class Email_Login extends AppCompatActivity {
         activity_email_login_actionbar=getSupportActionBar();
         activity_email_login_actionbar.setTitle("登錄頁面");
         activity_email_login_actionbar.setDisplayHomeAsUpEnabled(true);
+        email_login_edit_text=findViewById(R.id.email_login_edit_text);
+        password_login_edit_text=findViewById(R.id.password_login_edit_text);
+        confirm_button_of_activity_email_register=(Button)findViewById(R.id.confirm_button_of_activity_email_register);
+        confirm_button_of_activity_email_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(email_login_edit_text.getText().toString().matches("root")&&password_login_edit_text.getText().toString().matches("toor")){
+                    Toast.makeText(Email_Login.this,"登錄成功",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(Email_Login.this,"賬號或密碼輸入錯誤",Toast.LENGTH_SHORT).show();
+                    email_login_edit_text.requestFocus();
+                }
+            }
+        });
 
 
 
